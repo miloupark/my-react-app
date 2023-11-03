6ae4744741381cc30290019fb5e7caeb46a521f1
miloupark <helenapark96@naver.com>
2023-11-03 12:50:28 +0900

    !

--- src/App.js
+++ src/App.js
@@ -1,42 +1,11 @@
-function Header(props) {
-  return (
-    <header>
-      <h1>{props.title}</h1>
-      <Nav/>
-    </header>
-  )
-};
-// 컴포넌트(Header) 안에 컴포넌트(Nav)를 넣을 수 있음 상단 참고
-
-function Nav(props) {
-  const list = props.nav.map((item, index) => (
-    <li key={index}>
-      <a href={"/sub" + item.title}>{item.title}</a>
-    </li>
-  )
-);
-
-  return (
-    <nav>
-      <ul>{list}</ul>
-    </nav>
-  )
-};
-
-function Article (props){
-  return (
-    <article>
-      <h2>{props.title}</h2>
-      <p>{props.desc}</p>
-    </article>
-  )
-};
+import React from "react";
+import styled from 'styled-components';
 
 function App() {
   const navArr = [{title: "html"}, {title: "css"}, {title: "js"}];
   return (
     <div className="root"> 
-      <Header title="리액트(React)"/>
+      <HeaderStyled title="리액트(React)" nav={navArr}/>
       <Nav nav={navArr}/>
       <Article title="리액트란?" desc="desc 1"/>
       <Article title="컴포넌트란?" desc="desc 2"/>
@@ -44,5 +13,44 @@ function App() {
     </div>
   );
 }
+
+function Header({ title, nav }) {
+  // const { title, nav } = props;
+  return (
+    <header>
+      <h1>{title}</h1>
+      <Nav nav={nav}/>
+    </header>
+  );
+}
+
+function Nav({ nav }){
+  // const { nav } = props;
+  return (
+    <nav>
+      <ul>
+        {nav.map((item, index) => (
+          <li key={index}>
+            <a href={"/sub" + item.title}>{item.title}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+}
+
+function Article(props){
+  return (
+    <article>
+      <h2>{props.title}</h2>
+      <p>{props.desc}</p>
+    </article>
+  );
+}
+
+const HeaderStyled = styled(Header)`
+  display: flex;
+  border-bottom: 1px solid #eee;
+`;
 
 export default App;
